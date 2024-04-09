2ae58f82fb2aea6b3365d664ca217b6a39e33a55
Logan Prust <ljprust@uwm.edu>
2024-04-09 16:41:04 -0700

    removed erroneous C++ stuff

--- Initial/gabefit.c
+++ Initial/gabefit.c
@@ -2,20 +2,38 @@
 #include "../defs.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <math.h>
 
-static double vmax   = 0.0;
-static double rhoISM = 0.0;
-static double Eej    = 0.0;
-static double Mej    = 0.0;
-static double vwind  = 0.0;
-static double Mdot   = 0.0;
-static double t0     = 0.0;
-static double Msun   = 0.0;
-static double yr     = 0.0;
-static double day    = 0.0;
-static double Lz     = 0.0;
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+
+static double vmax     = 0.0;
+static double rhoISM   = 0.0;
+static double Eej      = 0.0;
+static double Mej      = 0.0;
+static double vwind    = 0.0;
+static double Mdot     = 0.0;
+static double t0       = 0.0;
+static double Msun     = 0.0;
+static double yr       = 0.0;
+static double day      = 0.0;
+static double Lz       = 0.0;
 static bool   quadrant = false;
 static double thetamin = 0.0;
+
+double outerfit( double th, double thpeak, double thbow, double rad );
+double innerfit( double th, double threcomppeak, double rad );
+double transfit( double th, double thetaL, double thetaR, double rhoL, double rhoR );
+double rmax( double theta, double rad0 );
+double afit( double radius );
+double dfit( double radius );
+double c1fit( double radius );
+double c2fit( double radius );
+double c3fit( double radius );
+double thetabowfit( double radius );
+double thetapeakfit( double radius );
+double thetarecompfit( double radius );
+double thetarecomppeakfit( double radius );
 
 void setICParams( struct domain * theDomain ){
    // constants
@@ -38,6 +56,108 @@ void setICParams( struct domain * theDomain ){
 
    // model a quadrant of the cube or just an octant
    quadrant = true;
+
+   double afit( double radius );
+}
+
+double afit( double radius ) {
+  double AU = 1.496e13;
+  double rad = radius/AU;
+  double a = 1.03125262-3.59159214*rad+5.7664357*rad*rad-4.43428767*rad*rad*rad+1.41344816*rad*rad*rad*rad;
+  return a;
+}
+
+double dfit( double radius ) {
+  double d = exp(-0.25425228*log(radius)+8.36636415);
+  return d;
+}
+
+double c1fit( double radius ) {
+  double AU = 1.496e13;
+  double rad = radius/AU;
+  double c1 = -0.46218021-3.2527453*rad+2.22623454*rad*rad+0.31*rad*rad*rad-0.0600881*rad*rad*rad*rad;
+  return c1;
+}
+
+double c2fit( double radius ) {
+  double c2 = -0.50302244*log(radius)+15.95891016;
+  return c2;
+}
+
+double c3fit( double radius ) {
+  double c3 = -0.06285498*log(radius)+1.93430793;
+  return c3;
+}
+
+double thetabowfit( double radius ) {
+  double th = exp(-0.29336977*log(radius)+8.56898219);
+  return th;
+}
+
+double thetapeakfit( double radius ) {
+  double th = exp(-0.30071382*log(radius)+8.75640844);
+  return th;
+}
+
+double thetarecompfit( double radius ) {
+  double th = exp(-0.19975411*log(radius)+4.84977536);
+  return th;
+}
+
+double thetarecomppeakfit( double radius ) {
+  double th = exp(-0.24091959*log(radius)+6.01677967);
+  return th;
+}
+
+double outerfit( double th, double thpeak, double thbow, double rad ) {
+   double a, d, result;
+   a = afit(rad);
+   d = dfit(rad);
+   result = a + 20.0 * sin( th/thbow*1.015*3.14159 ) * pow( th/thpeak, 16.0*d )
+          + d * pow( th/thpeak, 2.75*d );
+   if (th > thpeak) {
+      result = MAX(result, 1.0);
+   }
+   return result;
+}
+
+double innerfit( double th, double threcomppeak, double rad ) {
+   double c1, c2, c3, y;
+   c1 = c1fit(rad);
+   c2 = c2fit(rad);
+   c3 = c3fit(rad);
+   y = log(th/threcomppeak);
+   return exp( c1 + c2*y + c3*y*y );
+}
+
+double transfit( double th, double thetaL, double thetaR, double rhoL, double rhoR ) {
+   double rhomax, rhomin, tanharg;
+   rhomax = MAX(rhoL, rhoR);
+   rhomin = MIN(rhoL, rhoR);
+   tanharg = -3.0*((th-thetaL)/(thetaR-thetaL)*2.0-1.0);
+   return (tanh(tanharg) + 1.0) * 0.5 * (rhomax-rhomin) + rhomin;
+}
+
+double rmax( double theta, double rad0 ) {
+   double theta1, theta2, theta3, rad1, rad2, rad3, maxr;
+   theta1 = 0.205;
+   theta2 = 0.28;
+   theta3 = 0.6;
+   rad1 = 1.44516;
+   rad2 = 1.5484;
+   rad3 = 1.2258;
+
+   if ( theta<theta1 ) {
+      maxr = rad1 + 2.4563*theta*theta;
+   } else if ( theta<theta2 ) {
+      maxr = -4.30108*theta + 2.430108;
+   } else if ( theta<theta3 ) {
+      maxr = -2.20514*(theta-theta2)*(theta-theta2) + rad3;
+   } else {
+      maxr = 1.0;
+   }
+   maxr *= rad0;
+   return maxr;
 }
 
 void initial( double * prim , double * xi , double t , bool debug ){
@@ -61,7 +181,7 @@ void initial( double * prim , double * xi , double t , bool debug ){
    theta = acos(z/r);
    thetaDeg = theta*180.0/3.14159;
    r_max = rmax(theta, r0);
-   theta = max(theta, thetamin);
+   theta = MAX(theta, thetamin);
 
    thetabow        = thetabowfit(r);
    thetapeak       = thetapeakfit(r);
@@ -113,102 +233,3 @@ void initial( double * prim , double * xi , double t , bool debug ){
    prim[PPP] = 1.0e-5*vmax*vmax*prim[RHO];
 }
 
-double outerfit( double th, double thpeak, double thbow, double rad ) {
-   double a, d, result;
-   a = afit(rad);
-   d = dfit(rad);
-   result = a + 20.0 * sin( th/thbow*1.015*3.14159 ) * pow( th/thpeak, 16.0*d )
-          + d * pow( th/thpeak, 2.75*d );
-   if (th > thpeak) {
-      result = max(result, 1.0);
-   }
-   return result;
-}
-
-double innerfit( double th, double threcomppeak, double rad ) {
-   double c1, c2, c3, y;
-   c1 = c1fit(rad);
-   c2 = c2fit(rad);
-   c3 = c3fit(rad);
-   y = ln(th/threcomppeak);
-   return exp( c1 + c2*y + c3*y*y );
-}
-
-double transfit( double th, double thetaL, double thetaR, double rhoL, double rhoR ) {
-   double rhomax, rhomin, tanharg;
-   rhomax = max(rhoL, rhoR);
-   rhomin = min(rhoL, rhoR);
-   tanharg = -3.0*((th-thetaL)/(thetaR-thetaL)*2.0-1.0);
-   return (tanh(tanharg) + 1.0) * 0.5 * (rhomax-rhomin) + rhomin;
-}
-
-double rmax( double theta, double rad0 ) {
-   double theta1, theta2, theta3, rad1, rad2, rad3, maxr;
-   theta1 = 0.205;
-   theta2 = 0.28;
-   theta3 = 0.6;
-   rad0 = 1.44516;
-   rad1 = 1.5484;
-   rad2 = 1.2258;
-
-   if ( theta<theta1 ) {
-      maxr = rad0 + 2.4563*theta*theta;
-   } else if ( theta<theta2 ) {
-      maxr = -4.30108*theta + 2.430108;
-   } else if ( theta<theta3 ) {
-      maxr = -2.20514*(theta-theta2)*(theta-theta2) + rad2;
-   } else {
-      maxr = 1.0;
-   }
-   maxr *= rad0;
-   return maxr;
-}
-
-double afit( double radius ) {
-  double AU = 1.496e13;
-  double rad = radius/AU;
-  double a = 1.03125262-3.59159214*rad+5.7664357*rad*rad-4.43428767*rad*rad*rad+1.41344816*rad*rad*rad*rad;
-  return a;
-}
-
-double dfit( double radius ) {
-  double d = exp(-0.25425228*log(radius)+8.36636415);
-  return d;
-}
-
-double c1fit( double radius ) {
-  double AU = 1.496e13;
-  double rad = radius/AU;
-  double c1 = -0.46218021-3.2527453*rad+2.22623454*rad*rad+0.31*rad*rad*rad-0.0600881*rad*rad*rad*rad;
-  return c1;
-}
-
-double c2fit( double radius ) {
-  double c2 = -0.50302244*log(radius)+15.95891016;
-  return c2;
-}
-
-double c3fit( double radius ) {
-  double c3 = -0.06285498*log(radius)+1.93430793;
-  return c3;
-}
-
-double thetabowfit( double radius ) {
-  double th = exp(-0.29336977*log(radius)+8.56898219);
-  return th;
-}
-
-double thetapeakfit( double radius ) {
-  double th = exp(-0.30071382*log(radius)+8.75640844);
-  return th;
-}
-
-double thetarecompfit( double radius ) {
-  double th = exp(-0.19975411*log(radius)+4.84977536);
-  return th;
-}
-
-double thetarecomppeakfit( double radius ) {
-  double th = exp(-0.24091959*log(radius)+6.01677967);
-  return th;
-}
